2d126eb4d6d82ca2d8a23a693dd7c421896fd315
Danira Cortez <cortezd334@gmail.com>
2020-05-16 03:20:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -42,5 +42,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find {|element| element.include? valid_calls}
+  planeteer_calls.include? valid_calls}
 end
